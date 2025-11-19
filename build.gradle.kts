b3bd4e68ade0ba3a6725918e964ceb29ce66fd49
cronox3 <52287942+cronox3@users.noreply.github.com>
2025-11-19 18:04:06 -0500

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.0"
     application
     kotlin("plugin.serialization") version "1.9.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 repositories {
@@ -13,8 +14,15 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:2.3.5")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.5")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
 }
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    archiveBaseName.set("hash-api")
+    archiveClassifier.set("")
+    archiveVersion.set("")
 }
